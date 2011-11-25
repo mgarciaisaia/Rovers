e292c34a9de7885494092568a5651fbb11da78c8
Matias Garcia Isaia <mgarciaisaia+github@gmail.com>
2011-11-25 20:09:59 -0300

    Position: equals & hashCode

--- src/main/java/ar/com/unwebmaster/model/Position.java
+++ src/main/java/ar/com/unwebmaster/model/Position.java
@@ -18,4 +18,35 @@ public class Position {
 	public Grid ground() {
 		return ground;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((coordinate == null) ? 0 : coordinate.hashCode());
+		result = prime * result + ((ground == null) ? 0 : ground.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (coordinate == null) {
+			if (other.coordinate != null)
+				return false;
+		} else if (!coordinate.equals(other.coordinate))
+			return false;
+		if (ground == null) {
+			if (other.ground != null)
+				return false;
+		} else if (!ground.equals(other.ground))
+			return false;
+		return true;
+	}
 }
